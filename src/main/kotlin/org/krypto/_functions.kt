1d1d7b65495e4427facd0cef6237fe3f4a1df32b
Ryan Evans <rs3vans@gmail.com>
2016-07-20 11:14:49 -0500

    Add 'matchesPasswordHash' function and tests

--- src/main/kotlin/org/krypto/_functions.kt
+++ src/main/kotlin/org/krypto/_functions.kt
@@ -1,6 +1,8 @@
 package org.krypto
 
 import org.krypto.key.PasswordBasedKey
+import org.krypto.util.DEFAULT_RANDOM_BYTES_SIZE
+import org.krypto.util.decodBase64
 import org.krypto.util.encodeBase64String
 import org.krypto.util.generateRandomBytes
 
@@ -10,3 +12,15 @@ import org.krypto.util.generateRandomBytes
  */
 fun String.hashPassword(salt: ByteArray = generateRandomBytes()): String =
         (PasswordBasedKey(this.toCharArray(), salt = salt).bytes + salt).encodeBase64String()
+
+/**
+ * Checks to see if the given [hash] value matches the hashing of this string.
+ */
+fun String.matchesPasswordHash(hash: String, saltSize: Int = DEFAULT_RANDOM_BYTES_SIZE): Boolean {
+    val hashBytes = hash.decodBase64()
+    val salt = hashBytes.copyOfRange(hashBytes.size - saltSize, hashBytes.size)
+
+    val newHash = hashPassword(salt)
+
+    return newHash == hash
+}
---
+++ src/test/kotlin/org/krypto/passwords.kt
@@ -0,0 +1,28 @@
+package org.krypto
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class PasswordTests {
+
+    @Test
+    fun testHashPassword() {
+        val hash = PASSWORD.hashPassword(salt)
+
+        assertEquals(HASH, hash)
+    }
+
+    @Test
+    fun testMatchesPasswordHash() {
+        assertTrue(PASSWORD.matchesPasswordHash(HASH))
+    }
+
+    companion object {
+
+        const val PASSWORD = "really secure password YO!"
+        const val HASH = "v0jmlOb5PaAoJCV1Ci/5D9t/Wml5yOIx"
+
+        val salt = byteArrayOf(-37, 127, 90, 105, 121, -56, -30, 49)
+    }
+}
