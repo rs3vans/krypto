4d004a862c9bebf2052dedfe87ab6ec9b480cd76
Ryan Evans <rs3vans@gmail.com>
2016-07-20 11:14:31 -0500

    Cleanup Encrypted class

--- src/main/kotlin/org/krypto/Encrypted.kt
+++ src/main/kotlin/org/krypto/Encrypted.kt
@@ -3,16 +3,9 @@ package org.krypto
 import java.util.*
 
 /**
- * TODO
+ * Represents an encrypted output from a [Cipher].
  */
 data class Encrypted(val bytes: ByteArray, val iv: ByteArray) {
-
-    /**
-     * TODO
-     */
-    constructor(bytesWithIV: ByteArray) : this(
-            bytesWithIV.copyOfRange(0, bytesWithIV.size - IV_SIZE),
-            bytesWithIV.copyOfRange(bytesWithIV.size - IV_SIZE, bytesWithIV.size))
 
     val bytesWithIV = bytes + iv
 
@@ -21,13 +14,5 @@ data class Encrypted(val bytes: ByteArray, val iv: ByteArray) {
     override fun equals(other: Any?): Boolean = when (other) {
         is ByteArray -> Arrays.equals(bytesWithIV, other)
         else -> false
-    }
-
-    companion object {
-
-        /**
-         * TODO
-         */
-        const val IV_SIZE = 16
     }
 }
